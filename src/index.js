322a1484bea92fe08f1cc7185913f4f9abc82d9d
yoshiken0927 <51016793+yoshiken0927@users.noreply.github.com>
2021-02-01 23:30:31 +0900

    動いた

--- src/index.js
+++ src/index.js
@@ -2,13 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 //コンポーネントを定義
-var Index = React.createClass({
-  render:function(){
-    return (
-        <p>hoge</p>
-    );
-  }
-});
+function Index() {
+  return (
+    <p>hoge</p>
+  );
+};
 
 //id='content'の要素にコンポーネント「Index」を挿入してレンダリング
 ReactDOM.render(
